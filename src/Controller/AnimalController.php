7bedc466854bca89e4bc6ebd4edc9bcd39b7ff1f
eduard2236 <eduardcolmenares2236@gmail.com>
2023-01-19 22:30:42 +0100

    se crea la el el repositorio de raza

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -15,9 +15,42 @@ class AnimalController extends AbstractController
      */
     public function index(): Response {
          //cargar el repositorio desde doctrine
+         $em = $this->getDoctrine()->getManager();
          $animal_repo = $this->getDoctrine()->getRepository(Animal::class);
-        $animales = $animal_repo = $animal_repo->findAll();
+         //esto carga todos los datos de la base de datos
+         $animales = $animal_repo->findAll();
 
+        //con este solo se obtiene el primer dato que encuentre
+        $animal = $animal_repo->findBy([
+            'raza' => 'africana',
+        ],['id'=>'desc']);
+            var_dump($animal);
+
+        //query builder
+        $qb = $animal_repo->createQueryBuilder('a')
+                                                //->andWhere("a.raza = : raza")
+                                                //->seParameter('raza' , 'americana')
+                                                ->orderBy('a.id','desc')
+                                                ->getQuery();
+        $resulset = $qb->execute();
+       
+
+        //dql
+
+        $dql = "SELECT a FROM App\Entity\Animal a ORDER bY a.id DESC";
+        $query = $em->createQuery($dql);
+        $resulset = $query->execute();
+
+        //sql
+        $connection = $this->getDoctrine()->getConnection();
+        $sql = 'select * from animales order by id desc';
+        $prepare = $connection->query($sql);
+        $resulset = $prepare->fetchAll();
+        //repository
+        $animals = $animal_repo->findByRaza('DESC');
+        var_dump($animals);
+
+        
         return $this->render('animal/index.html.twig', [
             'controller_name' => 'AnimalController',
             'animales' => $animales
@@ -59,5 +92,47 @@ class AnimalController extends AbstractController
         }
 
         return new Response($Message);
+    }
+    public function update($id){
+        //cargar doctrine
+        $doctrine = $this->getDoctrine();
+        //cargar entityManager
+        $em = $doctrine->getManager();
+        //cargar repo animal
+        $animal_repo = $em->getRepository(Animal::class);
+        //find para conseguir el objeto
+        $animal = $animal_repo->find($id);
+
+        //comprobar si el objeto me llega
+        if(!$animal){
+            $message = 'el animal no existe en la bd';
+        }else{
+            //asignar los valores al objeto
+            $animal->setTipo("Perro $id");
+        //persistir en doctrine
+            $em->persist($animal);
+        //guardar en la base de datos
+            $em->flush();
+            $message = 'has actualizado el animal : '.$animal->getId();
+        }
+        
+
+        //respuesta
+        return new Response($message);
+
+    }
+
+    public function delete(Animal $animal){
+        $em = $this->getDoctrine()->getManager();
+
+        if($animal && is_object($animal)){
+            $em->remove($animal);
+            $em->flush();
+
+            $message = 'el animal ha sido borrado';
+        }else{
+            $message = 'el animalno existe';
+        }
+        return new Response($message);
     }
 }
